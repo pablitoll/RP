35874b651e85d2aa920f13f0d7ef56cad14f13de
PMV <pablo.maria.vidal@gmail.com>
2019-02-12 15:31:45 -0300

    .

---
+++ RPGeneradorDePrecios/src/ar/com/rollpaper/pricing/beans/ClienteLista.java
@@ -0,0 +1,20 @@
+package ar.com.rollpaper.pricing.beans;
+
+public class ClienteLista {
+	
+	private CcobClie cliente;
+	private VentLipv lista;
+	public CcobClie getCliente() {
+		return cliente;
+	}
+	public void setCliente(CcobClie cliente) {
+		this.cliente = cliente;
+	}
+	public VentLipv getLista() {
+		return lista;
+	}
+	public void setLista(VentLipv lista) {
+		this.lista = lista;
+	}
+
+}
